aae52e19d8058faa27058cd472ee9fad572cae89
Max Sun <130513985+maxsunc@users.noreply.github.com>
2026-04-06 16:09:19 -0400

    Add: buy-and-sell-crypto-with-cooldown - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # cooldown period of one day
+
+        # [1,3,4,0,4]
+        # there is a one day cooldown
+
+        # keep track fo the buy index and the sell index
+
+        # when prices[sell] > prices[buy] then we are able to sell. we can choose to sell and skip one index looking for the next buy index
+        # return max()
+        # or choose to skip and wait for the next
+
+        # else prices[sell] <= prices[buy]:
+        # then set buy = sell since we wont gain anything from selling right now 
+        dp = [[-1 for i in range(0,len(prices))] for j in range(0,len(prices))]
+        def dfs(buy,sell):
+            if sell >= len(prices):
+                return 0
+
+            if dp[buy][sell] != -1:
+                return dp[buy][sell]            
+            val = 0
+            if prices[sell] > prices[buy]:
+                # we have the abilitiy to sell if we want
+                # print(f"{prices[sell]} {prices[buy]}")
+                val = max((prices[sell] - prices[buy] ) + dfs(sell + 2, sell + 3), dfs(buy, sell + 1))
+            else:
+                # print(f"{prices[sell]} less than {prices[buy]}")
+                # we can't sell since buy is greater than us
+                buy = sell # found a new min
+                val = dfs(buy, sell + 1)
+            dp[buy][sell] = val
+            return val
+
+        return dfs(0,0)
+
+
+        # [1,4,3,0,4]
