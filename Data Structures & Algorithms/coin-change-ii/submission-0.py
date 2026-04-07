8b7d23a0bcf28e55ac741e7462f157dee11435b9
Max Sun <130513985+maxsunc@users.noreply.github.com>
2026-04-07 10:04:10 -0400

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        # Diff: distinct combination of coins that add up to amount
+        # Diff: Unlimited coins
+
+        # backtracking: Try every single 
+        memo = {}
+        # either take or stop taking 
+
+        def dfs(i, curSum):
+            # base case
+            if curSum >= amount:
+                return 1 if curSum == amount else 0
+            if i >= len(coins):
+                return 0
+            
+            if (i,curSum) in memo:
+                return memo[(i,curSum)]
+            
+            # take or move onto next
+            val = dfs(i, curSum + coins[i]) + dfs(i+1,curSum) # ways to get tto the result both ways here
+
+            memo[(i,curSum)] = val
+            return val
+        return dfs(0,0)
+
+
+
