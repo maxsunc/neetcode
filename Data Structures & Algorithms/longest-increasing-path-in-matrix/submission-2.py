5f3974d68a20705b6476933bd7be82e90e52440e
Max Sun <130513985+maxsunc@users.noreply.github.com>
2026-04-08 20:24:03 -0400

    Add: longest-increasing-path-in-matrix - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-2.py
@@ -0,0 +1,50 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        # dfs at every single node
+        n = len(matrix)
+        m = len(matrix[0])
+        # with memoization so it goes faster
+
+        directions = [[1,0],[0,1],[-1,0],[0,-1]]
+        def isValid(i,j):
+            return 0 <= i < n and 0 <= j < m
+        memo = {}
+
+        # [1,2,3],
+        # [2,1,4],
+        # [7,6,5]
+        def dfs(i,j):
+            # base case: 
+            # if (i,j) in seen:
+            #     return 0
+            # seen.add((i,j))
+            if (i,j) in memo:
+                return memo[(i,j)]
+            length = 1
+
+            for dir in directions:
+                newI = i + dir[0]
+                newJ = j + dir[1]
+                if not isValid(newI,newJ):
+                    continue
+                if matrix[i][j] < matrix[newI][newJ]:
+                    # explore this branch and see how big it gets
+                    length = max(length, 1 + dfs(newI,newJ))
+                    # seen.remove((newI,newJ)) # backtracking step
+            memo[(i,j)] = length
+            return length
+        
+        maxVal = 0
+        for i in range(0,n):
+            for j in range(0,m):
+                maxVal = max(maxVal, dfs(i,j))
+
+                # seen.clear()
+        return maxVal
+
+
+
+        
+        # [1,2,3]
+        # [4,5,6]
+        # [7,8,9]
