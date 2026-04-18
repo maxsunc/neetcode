ebb3a85beca636233d0341e01eb97e29536aa88d
Max Sun <130513985+maxsunc@users.noreply.github.com>
2026-04-18 16:12:38 -0400

    Add: course-schedule - submission-8

---
+++ Data Structures & Algorithms/course-schedule/submission-8.py
@@ -0,0 +1,58 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # [0,1] => 1 is prereq to 0
+        # there are 0 to numCourses - 1 needed to take
+
+
+        # we want to see if its possible to finish all courses?
+
+        # just try to prove its false
+
+        # when is it impossible finish all courses
+        # interdepencides on each other: 
+
+        # [0,1]
+        # [1,0]
+        # cycle detection: dfs algorithm is detect a cycle
+        # build an adjacency list to traverse the value
+
+        # to catch a cycle we need to explore every single node 
+
+        # clear the seen set each time
+
+        # directed graph => 
+        # prereqs of each ccharacter
+        adjList = {i: [] for i in range(numCourses)}
+
+        # fill in the adjacency list using prerequisites
+        for p in prerequisites:
+            i,val = p[0],p[1]
+            adjList[i].append(val)
+        seen = set()
+        # 2. call dfs on each course from 0 to numCOurses checking for already seen courses
+        def dfsCanTake(i):
+            if i in seen:
+                # this is a cycle detected
+                return False
+            if adjList[i] == []:
+                return True
+            seen.add(i)
+
+            # check whether we can take prerequisites of this course (if we can then we retunr true)
+            pre = adjList[i]
+            for val in pre:
+                if not dfsCanTake(val):
+                    return False # we found a cycle
+            seen.remove(i)
+            # once its finished mark as finished
+            adjList[i] = []
+            return True
+        
+        for i in range(numCourses): 
+            if not dfsCanTake(i):
+                return False
+            # print(f"good for {i}")
+            # seen.clear()
+
+        return True
+        
