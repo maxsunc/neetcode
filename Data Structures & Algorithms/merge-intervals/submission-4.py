a50eff8edd41e331a0164e15b6e218b4009aa39f
Max Sun <130513985+maxsunc@users.noreply.github.com>
2026-07-08 16:09:38 -0400

    Update: merge-intervals - submission-4

--- Data Structures & Algorithms/merge-intervals/submission-4.py
+++ Data Structures & Algorithms/merge-intervals/submission-4.py
@@ -1,23 +1,39 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # stack check if we can merge wit hthe most recent value.
-        # we can only merge if theres an intersection
-        intervals.sort(key=lambda x: x[0])
-        print(intervals)
+        # merge all overlapping intervals and return an array of the non-overlapping intervals that cover all intervals in the input
+        
+
+        # they're not sorted intervals
+
+        # sort the intervals by start vlaue
+
+        # [[1,1000],[4,5],[3,1001]] = [[1,1001]]
+
+
+        # have a stack: iterate through intervals:
+        # foreach element 
+        # if our stack is emptyjust append it
+        # if its not empty check the top valiue of thestack 
+        # does it overlap with the currentInterval? If it does then pop, merge
+        # if it doesnt overlap then just break out of the loop and add
+        intervals = sorted(intervals,  key=lambda x: x[0])
         stack = []
 
-        for i,interval in enumerate(intervals):
-            if not stack:
-                stack.append(interval)
-            else:
-                topVal = stack[-1][1]
-                topVal2 = interval[1]
-                if interval[0] <= topVal <= interval[1]  or stack[-1][0] <= topVal2 <= stack[-1][1]:
-                    # merge
-                    stack[-1] = [min(interval[0],stack[-1][0]),max(interval[1], stack[-1][1])]
+        for interval in intervals:
+            # if not stack:
+            #     stack.append(interval)
+            #     continue
+            
+            newInterval = interval
+            # insert interval into stack
+            while stack:
+                curInterval = stack[-1]
+                if newInterval[0] <= curInterval[1]:
+                    curInterval = stack.pop()
+                    newInterval = [min(newInterval[0],curInterval[0]), max(newInterval[1],curInterval[1])]
                 else:
-                    # append as normal since no overlap
-                    stack.append(interval)
+                    break
+            # append the newInterval 
+            stack.append(newInterval)
         
         return stack
-
